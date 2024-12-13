6c7638be19f5c18edc94d3c0355fd6df9121894b
trantrongtin <trantin1973@gmail.com>
2024-12-13 19:31:14 +0700

    update file document for  BaiTap_Week5

--- BaiTap4_Payara_Week1/src/main/java/trantrongtin_iuh/baitap4_payara/backend/api/MyApplication.java
+++
@@ -1,9 +0,0 @@
-package trantrongtin_iuh.baitap4_payara.backend.api;
-
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class MyApplication extends Application {
-}
--- BaiTap_Week6/src/main/java/iuh/fit/baitap_week6/frontend/controllers/PostCommentControllder.java
+++
@@ -1,7 +0,0 @@
-package iuh.fit.baitap_week6.frontend.controllers;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class PostCommentControllder {
-}
